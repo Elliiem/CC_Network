d4faa2c9d74fc9efe8fc8646a70a35c862eaebab
Ellie Lapanje <ellie.lapanje@gmail.com>
2023-06-19 18:59:00 +0200

    pre UpdateSize rem

--- Array.lua
+++ Array.lua
@@ -49,21 +49,34 @@ function Array:At(index)
     end
 end
 
+function Array:Offset(index,offset)
+    self:UpdateSize()
+    for i=self.size, index, -1 do
+        self.data[i+offset] = self.data[i]
+        self.data[i] = nil
+    end
+end
+
 
 function Array:PushBack(data)
     self:UpdateSize()
     self.data[self.size+1] = data
 end
 
+function Array:Insert(index, val)
+    Array:Offset(index+1,1)
+    self.data[index+1] = val
+end
 
-function Array:Offset(index,offset)
-    self:UpdateSize()
-    for i=self.size, index, -1 do
-        print(i)
-    end
+function Array:Set(index, val)
+    self.data[index+1] = val
 end
 
 
 local array = Array:new({1,2,3,4})
 
-array:Offset(3,1)
+array:Insert(3,69)
+
+for i=0,10 do
+   print(array.data[i])
+end
